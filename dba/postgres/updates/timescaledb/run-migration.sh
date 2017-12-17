007c92d66f011dc4cca6ccd703e92ca239642a3a
Matt Magoffin <matt@solarnetwork.net>
2017-12-17 14:02:32 +1300

    Switch to production db name.

--- dba/postgres/updates/timescaledb/run-migration.sh
+++ dba/postgres/updates/timescaledb/run-migration.sh
@@ -4,11 +4,11 @@ pushd .
 cd init/updates
 
 echo `date` Starting NET-111 migration
-time psql96 -h tsdb -p 5432 -U solarnet -d solarnetwork -v ON_ERROR_STOP=1 -f NET-111-remove-domains.sql
+time psql96 -h postgres96 -p 5496 -U solarnet -d solarnetwork -v ON_ERROR_STOP=1 -f NET-111-remove-domains.sql
 echo `date` Finished NET-111 migration
 
 popd
 
-time psql96 -h tsdb -p 5432 -U solarnet -d solarnetwork -v ON_ERROR_STOP=1 -f NET-112-production.sql
+time psql96 -h postgres96 -p 5496 -U solarnet -d solarnetwork -v ON_ERROR_STOP=1 -f NET-112-production.sql
 
 echo $PWD
