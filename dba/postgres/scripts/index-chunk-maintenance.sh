7a07333bbd0883949c9be6c4117d4c0a00a5185d
Matt Magoffin <matt@solarnetwork.net>
2018-03-19 11:26:47 +1300

    Tweak hypertable chunk index maintenance to iterate over tables and perform maintenance one at a time, to try and avoid deadlocks when performing maintenance of all tables in one go.

---
+++ dba/postgres/scripts/index-chunk-maintenance.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Perform chunk index maintenance by iterating over the rows returned from
+# the find_chunk_index_need_maint() function and
+# calling perform_one_chunk_reindex_maintenance() on each of those.
+#
+# Note that the -n command line switch must be provided to actually perform
+# the maintenance, otherwise just the indexes requiring the maintenance will
+# be printed out.
+
+PSQL=$(which psql)
+PSQL_CONN_ARGS='-h tsdb -d solarnetwork -U postgres'
+CHUNK_MIN_AGE='1 week'
+CHUNK_MAX_AGE='24 weeks'
+REINDEX_MIN_AGE='11 weeks'
+NOT_DRY_RUN='FALSE'
+
+while getopts ":c:e:np:r:s:" opt; do
+	case $opt in
+		c) PSQL_CONN_ARGS="${OPTARG}";;
+		e) CHUNK_MAX_AGE="${OPTARG}";;
+		n) NOT_DRY_RUN='TRUE';;
+		p) PSQL="${OPTARG}";;
+		r) REINDEX_MIN_AGE="${OPTARG}";;
+		s) CHUNK_MIN_AGE="${OPTARG}";;
+		?)
+			echo "Unknown argument: ${OPTARG}"
+			exit 1
+	esac
+done
+shift $(($OPTIND - 1))
+
+maint_tables=$($PSQL -A -t ${PSQL_CONN_ARGS} -F ' ' -c "SELECT schema_name,table_name,index_name FROM _timescaledb_solarnetwork.find_chunk_index_need_maint(chunk_max_age => interval '${CHUNK_MAX_AGE}', chunk_min_age => interval '${CHUNK_MIN_AGE}', reindex_min_age => interval '${REINDEX_MIN_AGE}')")
+
+echo "$maint_tables" |while read c_schema c_table c_index; do
+	if [ -z "${c_schema}" ]; then
+		continue;
+	fi
+	if [ "${NOT_DRY_RUN}" = 'FALSE' ]; then
+		printf '[DRY RUN] '
+	fi
+	echo "Performing maintenance on ${c_schema}.${c_table} [${c_index}]"
+	$PSQL ${PSQL_CONN_ARGS} -c "SELECT * FROM _timescaledb_solarnetwork.perform_one_chunk_reindex_maintenance('${c_schema}','${c_table}','${c_index}',$NOT_DRY_RUN)"
+done
--- dba/postgres/setup/timescaledb/reindex-support.sql
+++ dba/postgres/setup/timescaledb/reindex-support.sql
@@ -38,21 +38,59 @@ ALTER TABLE _timescaledb_solarnetwork.chunk_time_index_maint
   OWNER TO solarnet;
 
 
-CREATE OR REPLACE FUNCTION _timescaledb_solarnetwork.perform_chunk_reindex_maintenance(
+/**
+ * Find all chunk indexes needing maintenance.
+ *
+ * @param chunk_max_age		the maximum age of a chunk to consider
+ * @param chunk_min_age		the minimum age of a chunk to consider
+ * @param redindex_min_age	the minimum interval before reindexing an index
+ */
+CREATE OR REPLACE FUNCTION _timescaledb_solarnetwork.find_chunk_index_need_maint(
     chunk_max_age interval DEFAULT interval '24 weeks',
     chunk_min_age interval DEFAULT interval '1 week',
-    reindex_min_age interval DEFAULT interval '11 weeks',
+    reindex_min_age interval DEFAULT interval '11 weeks'
+    )
+	RETURNS TABLE(schema_name name, table_name name, index_name name) LANGUAGE sql STABLE AS
+$$
+SELECT
+	chunk_schema_name,
+	chunk_table_name,
+	chunk_index_name
+FROM _timescaledb_solarnetwork.chunk_time_index_maint
+WHERE chunk_upper_range BETWEEN CURRENT_TIMESTAMP - chunk_max_age AND CURRENT_TIMESTAMP - chunk_min_age
+AND (chunk_index_last_reindex IS NULL OR chunk_index_last_reindex < CURRENT_TIMESTAMP - reindex_min_age)
+$$;
+
+ALTER FUNCTION _timescaledb_solarnetwork.find_chunk_index_need_maint(interval, interval, interval)
+  OWNER TO solarnet;
+
+
+/**
+ * Perform maintenance on one specific chunk table.
+ *
+ * @param chunk_schema		the name of the schema of the chunk
+ * @param chunk_table		the name of the chunk table
+ * @param chunk_index		the name of the chunk index
+ * @param not_dry_run		when false, do not perform actual maintenance, just return the indexes that match
+ */
+CREATE OR REPLACE FUNCTION _timescaledb_solarnetwork.perform_one_chunk_reindex_maintenance(
+    chunk_schema text,
+    chunk_table text,
+    chunk_index text,
     not_dry_run boolean DEFAULT FALSE
     )
-    RETURNS SETOF _timescaledb_solarnetwork.chunk_index_maint LANGUAGE plpgsql AS $$
+    RETURNS SETOF _timescaledb_solarnetwork.chunk_index_maint LANGUAGE plpgsql AS
+$$
 DECLARE
     rec _timescaledb_solarnetwork.chunk_index_maint%rowtype;
     mtn _timescaledb_solarnetwork.chunk_time_index_maint%rowtype;
 BEGIN
 	FOR mtn IN
 		SELECT * FROM _timescaledb_solarnetwork.chunk_time_index_maint
-		WHERE chunk_upper_range BETWEEN CURRENT_TIMESTAMP - chunk_max_age AND CURRENT_TIMESTAMP - chunk_min_age
-		AND (chunk_index_last_reindex IS NULL OR chunk_index_last_reindex < CURRENT_TIMESTAMP - reindex_min_age)
+		WHERE
+			chunk_schema_name = chunk_schema::name
+			AND chunk_table_name = chunk_table::name
+			AND chunk_index_name = chunk_Index::name
 	LOOP
 		IF not_dry_run THEN
 			RAISE NOTICE 'Reindexing chunk index % on table %', mtn.chunk_index_name, mtn.chunk_table_name;
@@ -85,7 +123,45 @@ BEGIN
 END
 $$;
 
-ALTER FUNCTION _timescaledb_solarnetwork.perform_chunk_reindex_maintenance(interval, interval, interval)
+ALTER FUNCTION _timescaledb_solarnetwork.perform_one_chunk_reindex_maintenance(text, text, text, boolean)
+  OWNER TO solarnet;
+
+
+/**
+ * Find all chunk indexes needing maintenance and perform the maintenance on them.
+ *
+ * @param chunk_max_age		the maximum age of a chunk to consider
+ * @param chunk_min_age		the minimum age of a chunk to consider
+ * @param redindex_min_age	the minimum interval before reindexing an index
+ * @param not_dry_run		when false, do not perform actual maintenance, just return the indexes that match
+ */
+CREATE OR REPLACE FUNCTION _timescaledb_solarnetwork.perform_chunk_reindex_maintenance(
+    chunk_max_age interval DEFAULT interval '24 weeks',
+    chunk_min_age interval DEFAULT interval '1 week',
+    reindex_min_age interval DEFAULT interval '11 weeks',
+    not_dry_run boolean DEFAULT FALSE
+    )
+    RETURNS TABLE(schema_name name, table_name name, index_name name) LANGUAGE plpgsql AS
+$$
+DECLARE
+    mtn RECORD;
+BEGIN
+	FOR mtn IN
+		SELECT * FROM _timescaledb_solarnetwork.find_chunk_index_need_maint(chunk_max_age, chunk_min_age, reindex_min_age)
+	LOOP
+		EXECUTE 'SELECT _timescaledb_solarnetwork.perform_one_chunk_reindex_maintenance($1,$2,$3)'
+		USING mtn.schema_name, mtn.table_name, mtn.index_name;
+
+		schema_name := mtn.schema_name;
+		table_name := mtn.table_name;
+		index_name := mtn.index_name;
+		RETURN NEXT;
+	END LOOP;
+	RETURN;
+END
+$$;
+
+ALTER FUNCTION _timescaledb_solarnetwork.perform_chunk_reindex_maintenance(interval, interval, interval, boolean)
   OWNER TO solarnet;
 
 /* Example call:
