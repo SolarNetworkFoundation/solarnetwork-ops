dd7b716095c9336d338eddd4f7429a525d8e11af
Matt Magoffin <matt@solarnetwork.net>
2019-02-16 16:12:07 +1300

    Discovered that REINDEX is not needed after a CLUSTER (https://dba.stackexchange.com/questions/39652/is-a-reindex-required-after-cluster). So eliminate the reindex and just to clustering.

--- dba/postgres/scripts/index-chunk-maintenance.sh
+++ dba/postgres/scripts/index-chunk-maintenance.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # Perform chunk index maintenance by iterating over the rows returned from
-# the find_chunk_index_need_reindex_maint() function and
-# calling perform_one_chunk_reindex_maintenance() on each of those, followed
-# by calling the find_chunk_index_need_cluster_maint() function and calling
+# the the find_chunk_index_need_cluster_maint() function and calling
 # perform_one_chunk_cluster_maintenance() on each of those.
 #
 # Note that the -n command line switch must be provided to actually perform
@@ -45,7 +43,7 @@ fi
 
 PAUSED=0
 
-maint_tables=$($PSQL -A -t ${PSQL_CONN_ARGS} -F ' ' -c "SELECT schema_name,table_name,index_name FROM _timescaledb_solarnetwork.find_chunk_index_need_reindex_maint(chunk_max_age => interval '${CHUNK_MAX_AGE}', chunk_min_age => interval '${CHUNK_MIN_AGE}', reindex_min_age => interval '${REINDEX_MIN_AGE}')")
+maint_tables=$($PSQL -A -t ${PSQL_CONN_ARGS} -F ' ' -c "SELECT schema_name,table_name,index_name FROM _timescaledb_solarnetwork.find_chunk_index_need_cluster_maint(chunk_max_age => interval '${CHUNK_MAX_AGE}', chunk_min_age => interval '${CHUNK_MIN_AGE}', reindex_min_age => interval '${REINDEX_MIN_AGE}')")
 
 while read c_schema c_table c_index; do
 	if [ -z "${c_schema}" ]; then
@@ -59,19 +57,6 @@ while read c_schema c_table c_index; do
 		do_job_status
 		wait_all_jobs_complete
 		PAUSED=1
-	fi
-	echo "Performing reindex maintenance on ${c_schema}.${c_table} [${c_index}]"
-	$PSQL ${PSQL_CONN_ARGS} -c "SELECT * FROM _timescaledb_solarnetwork.perform_one_chunk_reindex_maintenance('${c_schema}','${c_table}','${c_index}',$NOT_DRY_RUN)"
-done <<< "$maint_tables"
-
-maint_tables=$($PSQL -A -t ${PSQL_CONN_ARGS} -F ' ' -c "SELECT schema_name,table_name,index_name FROM _timescaledb_solarnetwork.find_chunk_index_need_cluster_maint(chunk_max_age => interval '${CHUNK_MAX_AGE}', chunk_min_age => interval '${CHUNK_MIN_AGE}', reindex_min_age => interval '${REINDEX_MIN_AGE}')")
-
-while read c_schema c_table c_index; do
-	if [ -z "${c_schema}" ]; then
-		continue;
-	fi
-	if [ "${NOT_DRY_RUN}" = 'FALSE' ]; then
-		printf '[DRY RUN] '
 	fi
 	echo "Performing cluster maintenance on ${c_schema}.${c_table} [${c_index}]"
 	$PSQL ${PSQL_CONN_ARGS} -c "SELECT * FROM _timescaledb_solarnetwork.perform_one_chunk_cluster_maintenance('${c_schema}','${c_table}','${c_index}',$NOT_DRY_RUN)"
