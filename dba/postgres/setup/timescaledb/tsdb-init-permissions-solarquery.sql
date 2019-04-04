1f08b887934a492090be815be20d01a8151b928f
Matt Magoffin <matt@solarnetwork.net>
2019-04-04 17:07:59 +1300

    OPS-3: add more permissions for the SolarQuery and SolarIn apps.

--- dba/postgres/setup/timescaledb/tsdb-init-permissions-solarquery.sql
+++ dba/postgres/setup/timescaledb/tsdb-init-permissions-solarquery.sql
@@ -1,3 +1,4 @@
--- SELECT on user tables for security enforcement
-GRANT SELECT ON solaruser.user_user TO solarquery;
-GRANT SELECT ON solaruser.user_node TO solarquery;
+GRANT SELECT ON TABLE solaruser.user_auth_token_login TO solarquery;
+GRANT SELECT ON TABLE solaruser.user_auth_token_role TO solarquery;
+GRANT SELECT ON TABLE solaruser.user_user TO solarquery;
+GRANT SELECT ON TABLE solaruser.user_node TO solarquery;
