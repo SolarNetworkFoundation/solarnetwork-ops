3ddd056aed85da8a42ea7c1517002fa39a5e60e5
Matt Magoffin <matt@solarnetwork.net>
2019-05-06 09:38:56 +1200

    OPS-4: integrate support for updating property files via environment variables in solarjobs app, to support Docker better.

--- virgo/example/apphome/solarjobs/bin/sn-start.sh
+++ virgo/example/apphome/solarjobs/bin/sn-start.sh
@@ -17,14 +17,27 @@ done
 SCRIPT_DIR=`dirname $SCRIPT`
 EXECUTABLE="dmk.sh"
 
-#
-# identify yourself when running under cygwin
-#
-cygwin=false
-case "$(uname)" in
-    CYGWIN*) cygwin=true ;;
-esac
-export cygwin
+export cygwin=false
+
+env_jdbc () {
+	local args=""
+	
+	if [ -n "$SN_JDBC_URL" ]; then
+		args="$args --set jdbc.url=$SN_JDBC_URL"
+	fi
+	
+	if [ -n "$SN_JDBC_USER" ]; then
+		args="$args --set jdbc.user=$SN_JDBC_USER"
+	fi
+	
+	if [ -n "$SN_JDBC_PASS" ]; then
+		args="$args --set jdbc.pass=$SN_JDBC_PASS"
+	fi
+	
+	java -jar "$SCRIPT_DIR"/sn-prop-util.jar modify --file "$SCRIPT_DIR"/../repository/etc/net.solarnetwork.central.dao.jdbc.properties $args
+}
+
+env_jdbc
 
 export JMX_PORT=9880
 echo "Starting Virgo HTTP on port 9080, debug port 9980."
