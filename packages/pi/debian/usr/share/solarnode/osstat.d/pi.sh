ff6da4a39f59cfe27f94a764db16702735f90de7
Matt Magoffin <matt@solarnetwork.net>
2019-09-06 06:53:45 +1200

    Add implementation for Pi cpu_temp using the vcgencmd program.

--- packages/pi/debian/usr/share/solarnode/osstat.d/pi.sh
+++ packages/pi/debian/usr/share/solarnode/osstat.d/pi.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 do_cpu_temp () {
-	
+	if [ -x /opt/vc/bin/vcgencmd ]; then
+		local temp=$(/opt/vc/bin/vcgencmd measure_temp 2>/dev/null |tr -cd 0123456789.-)
+		if [ -n "$temp" ]; then
+			echo 'i/cpu_temp'
+			echo "$temp"
+		fi
+	fi
 }
