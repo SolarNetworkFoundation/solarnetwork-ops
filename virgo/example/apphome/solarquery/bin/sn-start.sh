f132ea5e0e520c54079c306789368b7859e55a53
Matt Magoffin <matt@solarnetwork.net>
2020-03-11 10:09:19 +1300

    Update SolarQuery start script to support environment database settings.

--- virgo/example/apphome/solarquery/bin/sn-start.sh
+++ virgo/example/apphome/solarquery/bin/sn-start.sh
@@ -16,8 +16,52 @@ done
 
 SCRIPT_DIR=`dirname $SCRIPT`
 EXECUTABLE="dmk.sh"
+JDBC_CONF="$SCRIPT_DIR/../configuration/services/net.solarnetwork.jdbc.pool.hikari-central.cfg"
+JDBC_CONF_RW="$SCRIPT_DIR/../configuration/services/net.solarnetwork.jdbc.pool.hikari-central-readwrite.cfg"
 
 export cygwin=false
+
+env_jdbc () {
+	local args=""
+	
+	if [ -n "$SN_JDBC_URL" ]; then
+		args="$args --set jdbc.url=$SN_JDBC_URL"
+	fi
+	
+	if [ -n "$SN_JDBC_USER" ]; then
+		args="$args --set jdbc.user=$SN_JDBC_USER"
+	fi
+	
+	if [ -n "$SN_JDBC_PASS" ]; then
+		args="$args --set jdbc.pass=$SN_JDBC_PASS"
+	fi
+	
+	java -jar "$SCRIPT_DIR"/sn-prop-util.jar modify --file "$JDBC_CONF" $args
+}
+
+env_jdbc_rw () {
+	local args=""
+	
+	if [ -n "$SN_JDBC_URL_RW" ]; then
+		args="$args --set jdbc.url=$SN_JDBC_URL_RW"
+	fi
+	
+	if [ -n "$SN_JDBC_USER" ]; then
+		args="$args --set jdbc.user=$SN_JDBC_USER_RW"
+	fi
+	
+	if [ -n "$SN_JDBC_PASS" ]; then
+		args="$args --set jdbc.pass=$SN_JDBC_PASS_RW"
+	fi
+	
+	java -jar "$SCRIPT_DIR"/sn-prop-util.jar modify --file "$JDBC_CONF_RW" $args
+}
+
+env_jdbc
+
+if [ -e "$JDBC_CONF_RW" ]; then
+	env_jdbc_rw
+fi
 
 export JAVA_OPTS="-Djdk.management.heapdump.allowAnyFileSuffix=true -Dsolarnetwork.pidfile=$SCRIPT_DIR/../work/solarnet.pid -Dfelix.fileinstall.dir=$SCRIPT_DIR/../configuration/services -Dfelix.fileinstall.filter=.*\.cfg -Dfelix.fileinstall.noInitialDelay=true"
 export JMX_PORT=9882
