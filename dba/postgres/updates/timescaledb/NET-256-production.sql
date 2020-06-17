fc1ab7061bcbadef8adaa56ff5493e215fc81796
Matt Magoffin <matt@solarnetwork.net>
2020-06-17 22:37:24 +1200

    NET-256: add permission for SolarUser to access node hooks table.

--- dba/postgres/updates/timescaledb/NET-256-production.sql
+++ dba/postgres/updates/timescaledb/NET-256-production.sql
@@ -1,3 +1,6 @@
-GRANT ALL ON TABLE solaruser.user_node_event_task TO solarjobs;
-GRANT ALL ON TABLE solaruser.user_node_event_task_result TO solarjobs;
-GRANT SELECT ON TABLE solaruser.user_node_event_hook TO solarjobs;
+GRANT ALL    ON TABLE    solaruser.user_node_event_task        TO solarjobs;
+GRANT ALL    ON TABLE    solaruser.user_node_event_task_result TO solarjobs;
+GRANT SELECT ON TABLE    solaruser.user_node_event_hook        TO solarjobs;
+
+GRANT ALL    ON TABLE    solaruser.user_node_event_hook        TO solaruser;
+GRANT ALL    ON SEQUENCE solaruser.user_node_event_hook_seq    TO solaruser;
