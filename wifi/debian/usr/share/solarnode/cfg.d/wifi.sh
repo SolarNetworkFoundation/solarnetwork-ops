2989d0b04908b5acd94c8c877a1fdbb50e6075c2
Matt Magoffin <matt@solarnetwork.net>
2020-06-22 16:43:12 +1200

    NODE-184: add supports for settings, status, and reset sn-wifi actions.

--- wifi/debian/usr/share/solarnode/cfg.d/wifi.sh
+++ wifi/debian/usr/share/solarnode/cfg.d/wifi.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 #
 # Helper script for SolarNode OS WiFi configuration support.
+#
+# The configure action takes country, ssid, and password values and configures
+# the /etc/wpa_supplicant-wlan0.conf file and associated debconf settings for
+# the sn-wifi package.
+#
+# The settings action prints the country and SSID as lines to STDOUT.
+#
+# The status action prints "active" or "inactive" on a line, followed by all
+# available non-link IP addresses on lines.
 
 if [ $# -lt 1 ]; then
 	echo "Must provide action argument."  1>&2
@@ -70,8 +79,39 @@ EOF
 	rm $tmpfile
 }
 
+# settings: print country, ssid on lines to STDOUT
+do_settings () {
+	# capture to local variables in case no value exists in debconf; still need to print
+	# exactly the expected lines
+	local country=$(debconf-show sn-wifi |awk -F: '/sn-wifi\/country/{gsub(/^ +/, "", $2); print $2}')
+	local ssid=$(debconf-show sn-wifi |awk -F: '/sn-wifi\/ssid/{gsub(/^ +/, "", $2); print $2}')
+	echo "$country"
+	echo "$ssid"
+}
+
+# print out connection status, followed by non-link IP addresses
+do_status () {
+	if systemctl is-active wpa_supplicant@wlan0 >/dev/null; then
+		echo "active"
+	else
+		echo "inactive"
+	fi
+	ip -o addr list wlan0 |awk '$6 != "link" {gsub(/\/.+/, "", $4); print $4}'
+}
+
+# restart WiFi
+do_restart () {
+	systemctl restart wpa_supplicant@wlan0
+}
+
 case $ACTION in
 	configure) do_configure "$@";;
+
+	settings) do_settings "$0";;
+
+	status) do_status "$@";;
+
+	restart) do_restart "$@";;
 
 	*)
 		echo "Action '${ACTION}' not supported." 1>&2
