9dfa0e7da82bc1b821ad14381b6880fc0a181dec
Matt Magoffin <matt@solarnetwork.net>
2020-06-24 09:22:24 +1200

    NODE-185: add reset support for WiFi configuration.

---
+++ wifi/debian/usr/share/solarnode/reset.d/sn-wifi.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+#
+# Helper script for SolarNodeOS reset support.
+# 
+# If -a is passed as an argument, this script does nothing. Otherwise
+# it will delete the WiFi 
+# Executes executable scripts in /usr/share/solarnode/reset.d,
+# passing -a if that is passed to this script.
+
+APP_ONLY=""
+CONF_DIR="${CONF_DIR:-/etc/wpa_supplicant}"
+
+while getopts ":a" opt; do
+	case $opt in
+		a) APP_ONLY="-a";;
+		*)
+			echo "Unknown option '$OPTARG'." 1>&2
+			exit 1
+	esac
+done
+
+if [ -n "${APP_ONLY}" ]; then
+	exit 0
+fi
+
+if systemctl is-active sn-wifi-conf@wlan0.service >/dev/null; then
+	echo 'Stopping sn-wifi-conf@wlan0.service...'
+	systemctl stop sn-wifi-conf@wlan0.service || true
+fi
+if systemctl is-enabled sn-wifi-conf@wlan0.service >/dev/null; then
+	echo 'Disabling sn-wifi-conf@wlan0.service...'
+	systemctl disable sn-wifi-conf@wlan0.service || true
+fi
+
+echo 'Removing WiFi connection configuration...'
+rm -vf "${CONF_DIR}"/wpa_supplicant@*.conf
