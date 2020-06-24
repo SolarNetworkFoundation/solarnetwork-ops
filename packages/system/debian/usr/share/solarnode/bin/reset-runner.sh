f1ca84b881fc5ecd07d461c05975e92ad9eb999e
Matt Magoffin <matt@solarnetwork.net>
2020-06-24 12:51:08 +1200

    NODE-185: rework reset to use systemd service, because systemd is killing child processes launched via direct shell execution used previously.

--- packages/system/debian/usr/share/solarnode/bin/reset-runner.sh
+++ packages/system/debian/usr/share/solarnode/bin/reset-runner.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env sh
 #
 # Helper script for SolarNodeOS reset support.
-#
+# 
 # Executes executable scripts in /usr/share/solarnode/reset.d,
-# passing -a if that is passed to this script. Pass -f to run 
-# in the foreground, otherwise the script will run in the 
-# background.
+# passing -a if that is passed to this script.
 
 
 APP_ONLY=""
 HOOK_DIR="${HOOK_DIR:-/usr/share/solarnode/reset.d}"
 
 while getopts ":a" opt; do
-    case $opt in
+	case $opt in
 		a) APP_ONLY="-a";;
 		*)
 			echo "Unknown option '$OPTARG'." 1>&2
 			exit 1
-    esac
+	esac
 done
 
-find "${HOOK_DIR}" -type f -perm -500 -exec sudo {} "${APP_ONLY}" \;
+find "${HOOK_DIR}" -type f -perm -500 -exec sudo {} ${APP_ONLY} \;
