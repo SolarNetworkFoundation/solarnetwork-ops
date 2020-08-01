4516453daabd4e2d529c821ce83f9ddfd7a35fcc
Matt Magoffin <matt@solarnetwork.net>
2020-08-01 17:54:10 +1200

    OPS-21: check in last effort of Kill Bill update to 0.22. No longer going to use as of migration to SNF billing.

--- killbill/mk-sn-war.sh
+++ killbill/mk-sn-war.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-VER=0.18.20
+VER=0.22.10
 
 cp -va killbill-profiles-killbill-$VER.war sn-killbill-profiles-killbill-$VER.war
 
-# Replace invoice JAR with bugfix
-zip -vd sn-killbill-profiles-killbill-$VER.war 'WEB-INF/lib/killbill-invoice-*.jar'
+# Replace release JARs with custom versions
+zip -vd sn-killbill-profiles-killbill-$VER.war \
+    'WEB-INF/lib/killbill-subscription-*.jar' \
+    'WEB-INF/lib/killbill-invoice-*.jar'
 
 # Add custom JARs
 cd sn-war-root
