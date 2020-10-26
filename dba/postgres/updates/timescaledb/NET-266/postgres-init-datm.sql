31d83fbf94f16ef5488fb3eb633cf503b72bc0d1
Matt Magoffin <matt@solarnetwork.net>
2020-10-26 17:49:32 +1300

    NET-266: refine migration functions to work with normal insert of JSON datum.

--- dba/postgres/updates/timescaledb/NET-266/postgres-init-datm.sql
+++ dba/postgres/updates/timescaledb/NET-266/postgres-init-datm.sql
@@ -1,4 +1,4 @@
--- DROP SCHEMA IF EXISTS solardatm CASCADE;
+--DROP SCHEMA IF EXISTS solardatm CASCADE;
 CREATE SCHEMA IF NOT EXISTS solardatm;
 
 -- datum stream indirection table
@@ -35,7 +35,7 @@ CREATE TABLE solardatm.da_loc_datm_meta (
 CREATE TABLE solardatm.da_datm (
 	stream_id	UUID NOT NULL,
 	ts			TIMESTAMP WITH TIME ZONE NOT NULL,
-	posted		TIMESTAMP WITH TIME ZONE NOT NULL,
+	received	TIMESTAMP WITH TIME ZONE NOT NULL,
 	data_i		NUMERIC[],
 	data_a		NUMERIC[],
 	data_s		TEXT[],
@@ -46,7 +46,7 @@ CREATE TABLE solardatm.da_datm (
 CREATE UNIQUE INDEX IF NOT EXISTS da_datm_unq_reverse ON solardatm.da_datm (stream_id, ts DESC);
 
 /**
- * Migrate a single `da_datum` row into the `solardatm.da_datm` table.
+ * Migrate a JSON datum into the `solardatm.da_datm` table.
  *
  * This function accepts the "current" property name arrays as an optimisation when migrating
  * multiple rows of data. If the given datum has properties that are not found in any of the
@@ -56,17 +56,30 @@ CREATE UNIQUE INDEX IF NOT EXISTS da_datm_unq_reverse ON solardatm.da_datm (stre
  * `solardatm.da_datm_meta` table.
  *
  * @param sid 		the stream ID to use
- * @param d 		the datum to migrate
+ * @param ddate 	datum timestamp
+ * @param src		datum source ID
+ * @param rdate		datum received date
+ * @param jdata_i	instantaneous JSON properties
+ * @param jdata_a	accumulating JSON properties
+ * @param jdata_s	status JSON properties
+ * @param jdata_t	tag properties
  * @param p_i		the stream instantaneous property names; possibly appeneded to
  * @param p_a 		the stream accumulating property names; possible appended to
  * @param p_s		the stream status property names; possible appended to
  */
-CREATE OR REPLACE FUNCTION solardatm.migrate_datum(
-		sid 		UUID,
-		d 			solardatum.da_datum,
-		INOUT p_i 	TEXT[],
-		INOUT p_a 	TEXT[],
-		INOUT p_s 	TEXT[]
+CREATE OR REPLACE FUNCTION solardatm.migrate_datum_json(
+		sid 			UUID,
+		ddate			TIMESTAMP WITH TIME ZONE,
+		src				TEXT,
+		rdate			TIMESTAMP WITH TIME ZONE,
+		jdata_i			JSONB,
+		jdata_a			JSONB,
+		jdata_s			JSONB,
+		jdata_t			TEXT[],
+		INOUT p_i 		TEXT[],
+		INOUT p_a 		TEXT[],
+		INOUT p_s 		TEXT[],
+		OUT   is_ins 	BOOLEAN
 	) LANGUAGE plpgsql VOLATILE AS
 $$
 DECLARE
@@ -81,7 +94,7 @@ DECLARE
 	idx		INTEGER;
 BEGIN
 	-- copy instantaneous props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_i) LOOP
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_i) LOOP
 		idx := COALESCE(array_position(p_i, p.key), 0);
 		IF idx < 1 THEN
 			UPDATE solardatm.da_datm_meta SET names_i = CASE
@@ -96,14 +109,14 @@ BEGIN
 		BEGIN
 			v_i[idx] := p.value::numeric;
 		EXCEPTION WHEN others THEN
-			RAISE WARNING 'JSON value not numeric: node %, source %, ts %, i.key %, i.value %',
-				d.node_id, d.source_id, d.ts, p.key, p.value;
+			RAISE WARNING 'JSON value not numeric: stream %, source %, ts %, i.key %, i.value %',
+				sid, src, ddate, p.key, p.value;
 			v_i[idx] := NULL;
 		END;
 	END LOOP;
 
 	-- copy accumulating props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_a) LOOP
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_a) LOOP
 		idx := COALESCE(array_position(p_a, p.key), 0);
 		IF idx < 1 THEN
 			UPDATE solardatm.da_datm_meta SET names_a = CASE
@@ -117,14 +130,14 @@ BEGIN
 		BEGIN
 			v_a[idx] := p.value::numeric;
 		EXCEPTION WHEN others THEN
-			RAISE WARNING 'JSON value not numeric: node %, source %, ts %, i.key %, i.value %',
-				d.node_id, d.source_id, d.ts, p.key, p.value;
+			RAISE WARNING 'JSON value not numeric: stream %, source %, ts %, i.key %, i.value %',
+				sid, src, ddate, p.key, p.value;
 			v_i[idx] := NULL;
 		END;
 	END LOOP;
 
 	-- copy status props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_s) LOOP
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_s) LOOP
 		idx := COALESCE(array_position(p_s, p.key), 0);
 		IF idx < 1 THEN
 			UPDATE solardatm.da_datm_meta SET names_s = CASE
@@ -138,17 +151,152 @@ BEGIN
 		v_s[idx] := p.value;
 	END LOOP;
 
-	INSERT INTO solardatm.da_datm (stream_id, ts, posted, data_i, data_a, data_s, data_t)
+	INSERT INTO solardatm.da_datm (stream_id, ts, received, data_i, data_a, data_s, data_t)
 	VALUES (
 		sid
-		, d.ts
-		, d.posted
+		, ddate
+		, rdate
 		, CASE WHEN COALESCE(array_length(v_i, 1), 0) < 1 THEN NULL ELSE v_i END
 		, CASE WHEN COALESCE(array_length(v_a, 1), 0) < 1 THEN NULL ELSE v_a END
 		, CASE WHEN COALESCE(array_length(v_s, 1), 0) < 1 THEN NULL ELSE v_s END
-		, d.jdata_t
+		, jdata_t
 	)
-	ON CONFLICT DO NOTHING;
+	ON CONFLICT (stream_id, ts) DO UPDATE
+	SET received = EXCLUDED.received,
+		data_i = EXCLUDED.data_i,
+		data_a = EXCLUDED.data_a,
+		data_s = EXCLUDED.data_s,
+		data_t = EXCLUDED.data_t
+	RETURNING (xmax = 0)
+	INTO is_ins;
+END;
+$$;
+
+
+/**
+ * Migrate a JSON location datum into the `solardatm.da_datm` table.
+ *
+ * This function accepts the "current" property name arrays as an optimisation when migrating
+ * multiple rows of data. If the given datum has properties that are not found in any of the
+ * given name arrays, the name will be added and then the associated `solardatm.da_datm_meta`
+ * column updated to match. The name arrays are declared as `INOUT` so that the updated values
+ * can be saved and used again, helping reduce the amount of updates needed to the
+ * `solardatm.da_datm_meta` table.
+ *
+ * @param sid 		the stream ID to use
+ * @param ddate 	datum timestamp
+ * @param src		datum source ID
+ * @param rdate		datum received date
+ * @param jdata_i	instantaneous JSON properties
+ * @param jdata_a	accumulating JSON properties
+ * @param jdata_s	status JSON properties
+ * @param jdata_t	tag properties
+ * @param p_i		the stream instantaneous property names; possibly appeneded to
+ * @param p_a 		the stream accumulating property names; possible appended to
+ * @param p_s		the stream status property names; possible appended to
+ */
+CREATE OR REPLACE FUNCTION solardatm.migrate_loc_datum_json(
+		sid 			UUID,
+		ddate			TIMESTAMP WITH TIME ZONE,
+		src				TEXT,
+		rdate			TIMESTAMP WITH TIME ZONE,
+		jdata_i			JSONB,
+		jdata_a			JSONB,
+		jdata_s			JSONB,
+		jdata_t			TEXT[],
+		INOUT p_i 		TEXT[],
+		INOUT p_a 		TEXT[],
+		INOUT p_s 		TEXT[],
+		OUT   is_ins 	BOOLEAN
+	) LANGUAGE plpgsql VOLATILE AS
+$$
+DECLARE
+	-- property name arrays
+	p		RECORD;
+
+	-- property value arrays
+	v_i 	NUMERIC[];
+	v_a		NUMERIC[];
+	v_s		TEXT[];
+
+	idx		INTEGER;
+BEGIN
+	-- copy instantaneous props
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_i) LOOP
+		idx := COALESCE(array_position(p_i, p.key), 0);
+		IF idx < 1 THEN
+			UPDATE solardatm.da_loc_datm_meta SET names_i = CASE
+					WHEN COALESCE(array_position(names_i, p.key), 0) < 1 THEN array_append(names_i, p.key)
+					ELSE names_i
+					END
+			WHERE stream_id = sid
+			RETURNING names_i INTO p_i;
+			idx := array_position(p_i, p.key);
+		END IF;
+		-- catch cast exceptions: saw example of 'Infinity' string
+		BEGIN
+			v_i[idx] := p.value::numeric;
+		EXCEPTION WHEN others THEN
+			RAISE WARNING 'JSON value not numeric: stream %, source %, ts %, i.key %, i.value %',
+				sid, src, ddate, p.key, p.value;
+			v_i[idx] := NULL;
+		END;
+	END LOOP;
+
+	-- copy accumulating props
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_a) LOOP
+		idx := COALESCE(array_position(p_a, p.key), 0);
+		IF idx < 1 THEN
+			UPDATE solardatm.da_loc_datm_meta SET names_a = CASE
+					WHEN COALESCE(array_position(names_a, p.key), 0) < 1 THEN array_append(names_a, p.key)
+					ELSE names_a
+					END
+			WHERE stream_id = sid
+			RETURNING names_a INTO p_a;
+			idx := array_position(p_a, p.key);
+		END IF;
+		BEGIN
+			v_a[idx] := p.value::numeric;
+		EXCEPTION WHEN others THEN
+			RAISE WARNING 'JSON value not numeric: stream %, source %, ts %, i.key %, i.value %',
+				sid, src, ddate, p.key, p.value;
+			v_i[idx] := NULL;
+		END;
+	END LOOP;
+
+	-- copy status props
+	FOR p IN SELECT * FROM jsonb_each_text(jdata_s) LOOP
+		idx := COALESCE(array_position(p_s, p.key), 0);
+		IF idx < 1 THEN
+			UPDATE solardatm.da_loc_datm_meta SET names_s = CASE
+					WHEN COALESCE(array_position(names_s, p.key), 0) < 1 THEN array_append(names_s, p.key)
+					ELSE names_i
+					END
+			WHERE stream_id = sid
+			RETURNING names_s INTO p_s;
+			idx := array_position(p_s, p.key);
+		END IF;
+		v_s[idx] := p.value;
+	END LOOP;
+
+	INSERT INTO solardatm.da_datm (stream_id, ts, received, data_i, data_a, data_s, data_t)
+	VALUES (
+		sid
+		, ddate
+		, rdate
+		, CASE WHEN COALESCE(array_length(v_i, 1), 0) < 1 THEN NULL ELSE v_i END
+		, CASE WHEN COALESCE(array_length(v_a, 1), 0) < 1 THEN NULL ELSE v_a END
+		, CASE WHEN COALESCE(array_length(v_s, 1), 0) < 1 THEN NULL ELSE v_s END
+		, jdata_t
+	)
+	ON CONFLICT (stream_id, ts) DO UPDATE
+	SET received = EXCLUDED.received,
+		data_i = EXCLUDED.data_i,
+		data_a = EXCLUDED.data_a,
+		data_s = EXCLUDED.data_s,
+		data_t = EXCLUDED.data_t
+	RETURNING (xmax = 0)
+	INTO is_ins;
 END;
 $$;
 
@@ -170,6 +318,7 @@ DECLARE
 
 	d		solardatum.da_datum;
 	rcount 	BIGINT := 0;
+	is_ins	BOOLEAN;
 	curs 	NO SCROLL CURSOR FOR
 				SELECT * FROM solardatum.da_datum
 				WHERE node_id = node
@@ -195,121 +344,16 @@ BEGIN
 	LOOP
 		FETCH curs INTO d;
 		EXIT WHEN NOT FOUND;
-		SELECT * FROM solardatm.migrate_datum(sid, d, p_i, p_a, p_s)
-		INTO p_i, p_a, p_s;
-		rcount := rcount + 1;
+		SELECT * FROM solardatm.migrate_datum_json(sid, d.ts, d.source_id, d.posted,
+						d.jdata_i, d.jdata_a, d.jdata_s, d.jdata_t, p_i, p_a, p_s)
+		INTO p_i, p_a, p_s, is_ins;
+		IF is_ins THEN
+			rcount := rcount + 1;
+		END IF;
 	END LOOP;
 	CLOSE curs;
 
 	RETURN rcount;
-END;
-$$;
-
-
-/**
- * Migrate a single `da_loc_datum` row into the `solardatm.da_datm` table.
- *
- * This function accepts the "current" property name arrays as an optimisation when migrating
- * multiple rows of data. If the given datum has properties that are not found in any of the
- * given name arrays, the name will be added and then the associated `solardatm.da_datm_meta`
- * column updated to match. The name arrays are declared as `INOUT` so that the updated values
- * can be saved and used again, helping reduce the amount of updates needed to the
- * `solardatm.da_datm_meta` table.
- *
- * @param sid 		the stream ID to use
- * @param d 		the datum to migrate
- * @param p_i		the stream instantaneous property names; possibly appeneded to
- * @param p_a 		the stream accumulating property names; possible appended to
- * @param p_s		the stream status property names; possible appended to
- */
-CREATE OR REPLACE FUNCTION solardatm.migrate_loc_datum(
-		sid 		UUID,
-		d 			solardatum.da_loc_datum,
-		INOUT p_i 	TEXT[],
-		INOUT p_a 	TEXT[],
-		INOUT p_s 	TEXT[]
-	) LANGUAGE plpgsql VOLATILE AS
-$$
-DECLARE
-	-- property name arrays
-	p		RECORD;
-
-	-- property value arrays
-	v_i 	NUMERIC[];
-	v_a		NUMERIC[];
-	v_s		TEXT[];
-
-	idx		INTEGER;
-BEGIN
-	-- copy instantaneous props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_i) LOOP
-		idx := COALESCE(array_position(p_i, p.key), 0);
-		IF idx < 1 THEN
-			UPDATE solardatm.da_loc_datm_meta SET names_i = CASE
-					WHEN COALESCE(array_position(names_i, p.key), 0) < 1 THEN array_append(names_i, p.key)
-					ELSE names_i
-					END
-			WHERE stream_id = sid
-			RETURNING names_i INTO p_i;
-			idx := array_position(p_i, p.key);
-		END IF;
-		-- catch cast exceptions: saw example of 'Infinity' string
-		BEGIN
-			v_i[idx] := p.value::numeric;
-		EXCEPTION WHEN others THEN
-			RAISE WARNING 'JSON value not numeric: loc %, source %, ts %, i.key %, i.value %',
-				d.loc_id, d.source_id, d.ts, p.key, p.value;
-			v_i[idx] := NULL;
-		END;
-	END LOOP;
-
-	-- copy accumulating props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_a) LOOP
-		idx := COALESCE(array_position(p_a, p.key), 0);
-		IF idx < 1 THEN
-			UPDATE solardatm.da_loc_datm_meta SET names_a = CASE
-					WHEN COALESCE(array_position(names_a, p.key), 0) < 1 THEN array_append(names_a, p.key)
-					ELSE names_a
-					END
-			WHERE stream_id = sid
-			RETURNING names_a INTO p_a;
-			idx := array_position(p_a, p.key);
-		END IF;
-		BEGIN
-			v_a[idx] := p.value::numeric;
-		EXCEPTION WHEN others THEN
-			RAISE WARNING 'JSON value not numeric: loc %, source %, ts %, i.key %, i.value %',
-				d.loc_id, d.source_id, d.ts, p.key, p.value;
-			v_i[idx] := NULL;
-		END;
-	END LOOP;
-
-	-- copy status props
-	FOR p IN SELECT * FROM jsonb_each_text(d.jdata_s) LOOP
-		idx := COALESCE(array_position(p_s, p.key), 0);
-		IF idx < 1 THEN
-			UPDATE solardatm.da_loc_datm_meta SET names_s = CASE
-					WHEN COALESCE(array_position(names_s, p.key), 0) < 1 THEN array_append(names_s, p.key)
-					ELSE names_i
-					END
-			WHERE stream_id = sid
-			RETURNING names_s INTO p_s;
-			idx := array_position(p_s, p.key);
-		END IF;
-		v_s[idx] := p.value;
-	END LOOP;
-
-	INSERT INTO solardatm.da_datm (stream_id, ts, posted, data_i, data_a, data_s, data_t)
-	VALUES (
-		sid
-		, d.ts
-		, d.posted
-		, CASE WHEN COALESCE(array_length(v_i, 1), 0) < 1 THEN NULL ELSE v_i END
-		, CASE WHEN COALESCE(array_length(v_a, 1), 0) < 1 THEN NULL ELSE v_a END
-		, CASE WHEN COALESCE(array_length(v_s, 1), 0) < 1 THEN NULL ELSE v_s END
-		, d.jdata_t
-	)
-	ON CONFLICT DO NOTHING;
 END;
 $$;
 
@@ -331,6 +375,7 @@ DECLARE
 
 	d		solardatum.da_loc_datum;
 	rcount 	BIGINT := 0;
+	is_ins  BOOLEAN;
 	curs 	NO SCROLL CURSOR FOR
 				SELECT * FROM solardatum.da_loc_datum
 				WHERE loc_id = loc
@@ -356,12 +401,179 @@ BEGIN
 	LOOP
 		FETCH curs INTO d;
 		EXIT WHEN NOT FOUND;
-		SELECT * FROM solardatm.migrate_loc_datum(sid, d, p_i, p_a, p_s)
-		INTO p_i, p_a, p_s;
-		rcount := rcount + 1;
+		SELECT * FROM solardatm.migrate_loc_datum_json(sid, d.ts, d.source_id, d.posted,
+						d.jdata_i, d.jdata_a, d.jdata_s, d.jdata_t, p_i, p_a, p_s)
+		INTO p_i, p_a, p_s, is_ins;
+		IF is_ins THEN
+			rcount := rcount + 1;
+		END IF;
 	END LOOP;
 	CLOSE curs;
 
 	RETURN rcount;
+END;
+$$;
+
+
+/**
+ * Add or update a datum record. The data is stored in the `solardatm.da_datm` table.
+ *
+ * @param node 	the node ID
+ * @param src 	the source ID
+ * @param ts 	the datum timestamp
+ * @param rdate the date the datum was received by SolarNetwork
+ * @param jdata the datum JSON object (with jdata_i, jdata_a, jdata_s, and jdata_t properties)
+ * @param track if `TRUE` then also insert results of `solardatum.calculate_stale_datum()`
+ *                     into the `solaragg.agg_stale_datum` table and call
+ *                     `solardatum.update_datum_range_dates()` to keep the
+ *                     `solardatum.da_datum_range` table up-to-date
+ */
+CREATE OR REPLACE FUNCTION solardatm.store_datum(
+	node 			BIGINT,
+	src 			TEXT,
+	ddate 			TIMESTAMP WITH TIME ZONE,
+	rdate 			TIMESTAMP WITH TIME ZONE,
+	jdata 			TEXT,
+	track 			BOOLEAN DEFAULT TRUE)
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$$
+DECLARE
+	ts_crea 			TIMESTAMP WITH TIME ZONE 	:= COALESCE(ddate, now());
+	ts_recv 			TIMESTAMP WITH TIME ZONE	:= COALESCE(rdate, now());
+	jdata_json 			JSONB 						:= jdata::jsonb;
+	jdata_prop_count 	INTEGER 					:= solardatum.datum_prop_count(jdata_json);
+	ts_recv_hour 		TIMESTAMP WITH TIME ZONE 	:= date_trunc('hour', ts_recv);
+	is_insert 			BOOLEAN 					:= false;
+
+	sid 	UUID;
+
+	-- property name arrays
+	p_i		TEXT[];
+	p_a		TEXT[];
+	p_s		TEXT[];
+BEGIN
+	-- get, or create, stream ID
+	INSERT INTO solardatm.da_datm_meta (node_id, source_id)
+	VALUES (node, src)
+	ON CONFLICT (node_id, source_id) DO NOTHING
+	RETURNING stream_id, names_i, names_a, names_s
+	INTO sid, p_i, p_a, p_s;
+
+	IF NOT FOUND THEN
+		SELECT stream_id, names_i, names_a, names_s
+		FROM solardatm.da_datm_meta
+		WHERE node_id = node AND source_id = src
+		INTO sid, p_i, p_a, p_s;
+	END IF;
+
+	SELECT * FROM solardatm.migrate_datum_json(sid, ts_crea, src, ts_recv,
+					jdata_json->jdata_i,
+					jdata_json->jdata_a,
+					jdata_json->jdata_s,
+					solarcommon.json_array_to_text_array(jdata_json->jdata_t),
+					p_i, p_a, p_s)
+	INTO p_i, p_a, p_s, is_insert;
+
+	/*
+	INSERT INTO solaragg.aud_datum_hourly (
+		ts_start, node_id, source_id, datum_count, prop_count)
+	VALUES (ts_post_hour, node, src, 1, jdata_prop_count)
+	ON CONFLICT (node_id, ts_start, source_id) DO UPDATE
+	SET datum_count = aud_datum_hourly.datum_count + (CASE is_insert WHEN TRUE THEN 1 ELSE 0 END),
+		prop_count = aud_datum_hourly.prop_count + EXCLUDED.prop_count;
+
+	IF track THEN
+		INSERT INTO solaragg.agg_stale_datum (agg_kind, node_id, ts_start, source_id)
+		SELECT 'h' AS agg_kind, node_id, ts_start, source_id
+		FROM solardatum.calculate_stale_datum(node, src, cdate)
+		ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+
+		IF is_insert THEN
+			PERFORM solardatum.update_datum_range_dates(node, src, cdate);
+		END IF;
+	END IF;
+	*/
+END;
+$$;
+
+
+/**
+ * Add or update a location datum record. The data is stored in the `solardatm.da_datm` table.
+ *
+ * @param loc 	the location ID
+ * @param src 	the source ID
+ * @param ts 	the datum timestamp
+ * @param rdate the date the datum was received by SolarNetwork
+ * @param jdata the datum JSON object (with jdata_i, jdata_a, jdata_s, and jdata_t properties)
+ * @param track if `TRUE` then also insert results of `solardatum.calculate_stale_datum()`
+ *                     into the `solaragg.agg_stale_datum` table and call
+ *                     `solardatum.update_datum_range_dates()` to keep the
+ *                     `solardatum.da_datum_range` table up-to-date
+ */
+CREATE OR REPLACE FUNCTION solardatm.store_loc_datum(
+	loc 			BIGINT,
+	src 			TEXT,
+	ddate 			TIMESTAMP WITH TIME ZONE,
+	rdate 			TIMESTAMP WITH TIME ZONE,
+	jdata 			TEXT,
+	track 			BOOLEAN DEFAULT TRUE)
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$$
+DECLARE
+	ts_crea 			TIMESTAMP WITH TIME ZONE 	:= COALESCE(ddate, now());
+	ts_recv 			TIMESTAMP WITH TIME ZONE	:= COALESCE(rdate, now());
+	jdata_json 			JSONB 						:= jdata::jsonb;
+	jdata_prop_count 	INTEGER 					:= solardatum.datum_prop_count(jdata_json);
+	ts_recv_hour 		TIMESTAMP WITH TIME ZONE 	:= date_trunc('hour', ts_recv);
+	is_insert 			BOOLEAN 					:= false;
+
+	sid 	UUID;
+
+	-- property name arrays
+	p_i		TEXT[];
+	p_a		TEXT[];
+	p_s		TEXT[];
+BEGIN
+	-- get, or create, stream ID
+	INSERT INTO solardatm.da_loc_datm_meta (loc_id, source_id)
+	VALUES (loc, src)
+	ON CONFLICT (loc_id, source_id) DO NOTHING
+	RETURNING stream_id, names_i, names_a, names_s
+	INTO sid, p_i, p_a, p_s;
+
+	IF NOT FOUND THEN
+		SELECT stream_id, names_i, names_a, names_s
+		FROM solardatm.da_loc_datm_meta
+		WHERE loc_id = loc AND source_id = src
+		INTO sid, p_i, p_a, p_s;
+	END IF;
+
+	SELECT * FROM solardatm.migrate_loc_datum_json(sid, ts_crea, src, ts_recv,
+					jdata_json->jdata_i,
+					jdata_json->jdata_a,
+					jdata_json->jdata_s,
+					solarcommon.json_array_to_text_array(jdata_json->jdata_t),
+					p_i, p_a, p_s)
+	INTO p_i, p_a, p_s, is_insert;
+
+	/*
+	INSERT INTO solaragg.aud_datum_hourly (
+		ts_start, loc_id, source_id, datum_count, prop_count)
+	VALUES (ts_post_hour, loc, src, 1, jdata_prop_count)
+	ON CONFLICT (loc_id, ts_start, source_id) DO UPDATE
+	SET datum_count = aud_datum_hourly.datum_count + (CASE is_insert WHEN TRUE THEN 1 ELSE 0 END),
+		prop_count = aud_datum_hourly.prop_count + EXCLUDED.prop_count;
+
+	IF track THEN
+		INSERT INTO solaragg.agg_stale_datum (agg_kind, loc_id, ts_start, source_id)
+		SELECT 'h' AS agg_kind, loc_id, ts_start, source_id
+		FROM solardatum.calculate_stale_datum(loc, src, cdate)
+		ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+
+		IF is_insert THEN
+			PERFORM solardatum.update_datum_range_dates(loc, src, cdate);
+		END IF;
+	END IF;
+	*/
 END;
 $$;
