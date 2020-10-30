e8ed0289e08fc073373e9a30c49d7f02b3468105
Matt Magoffin <matt@solarnetwork.net>
2020-10-31 11:55:52 +1300

    NET-266: tweaks to rollup query.

--- dba/postgres/updates/timescaledb/NET-266/postgres-init-datm-query-agg.sql
+++ dba/postgres/updates/timescaledb/NET-266/postgres-init-datm-query-agg.sql
@@ -199,11 +199,11 @@ CREATE OR REPLACE FUNCTION solardatm.rollup_datm_for_time_span(
 		stream_id 	UUID,
 		ts_start	TIMESTAMP WITH TIME ZONE,
 		data_i		NUMERIC[],					-- array of instantaneous property average values
-		stat_i		NUMERIC[][],				-- array of instantaneous property [count,min,max] statistic tuples
 		data_a		NUMERIC[],					-- array of accumulating property clock difference values
-		read_a		NUMERIC[][],				-- array of accumulating property reading [start,finish,diff] tuples
 		data_s		TEXT[],						-- array of "last seen" status property values
-		data_t		TEXT[]						-- array of all tags seen over period
+		data_t		TEXT[],						-- array of all tags seen over period
+		stat_i		NUMERIC[][],				-- array of instantaneous property [count,min,max] statistic tuples
+		read_a		NUMERIC[][]					-- array of accumulating property reading [start,finish,diff] tuples
 	) LANGUAGE SQL STABLE ROWS 500 AS
 $$
 	-- grab raw data + reset records, constrained by stream/date range
@@ -215,41 +215,23 @@ $$
 			tolerance_read
 		)
 	)
-	-- calculate time-weights for data_i values per property
+	-- calculate instantaneous values per property
 	, wi AS (
 		SELECT
 			  p.idx AS idx
 			, p.val AS val
-			, CASE
-				WHEN d.ts < start_ts
-					THEN 0
-
-				WHEN lag(d.ts) OVER slot + tolerance_clock < start_ts
-					THEN 0
-
-				WHEN lag(d.ts) OVER slot < start_ts
-					THEN  EXTRACT(epoch FROM (d.ts - start_ts)) / EXTRACT(epoch FROM (d.ts - lag(d.ts) OVER slot))
-
-				WHEN d.ts > end_ts + tolerance_clock
-					THEN 0
-
-				WHEN d.ts > end_ts AND lag(d.ts) OVER slot < end_ts
-					THEN EXTRACT(epoch FROM (end_ts - lag(d.ts) OVER slot)) / EXTRACT(epoch FROM (d.ts - lag(d.ts) OVER slot))
-
-				ELSE
-					1
-				END AS portion
 		FROM d
 		INNER JOIN unnest(d.data_i) WITH ORDINALITY AS p(val, idx) ON TRUE
 		WHERE d.data_i IS NOT NULL
-		WINDOW slot AS (PARTITION BY p.idx ORDER BY d.ts RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+			AND d.ts >= start_ts
+			AND d.ts < end_ts
 	)
 	-- calculate instantaneous statistics
 	, di AS (
 		SELECT
 			w.idx
-			, to_char(avg(w.val * w.portion), 'FM999999999999999999990.999999999')::numeric AS val
-			, count(w.val) FILTER (WHERE portion > 0) AS cnt
+			, to_char(avg(w.val), 'FM999999999999999999990.999999999')::numeric AS val
+			, count(w.val) AS cnt
 			, min(val) AS val_min
 			, max(val) AS val_max
 		FROM wi w
@@ -277,7 +259,10 @@ $$
 				ELSE p.val - lag(p.val) OVER slice
 				END AS diff
 			, first_value(p.val) OVER slot AS rstart
-			, last_value(p.val) OVER slot AS rend
+			, CASE
+				WHEN row_number() OVER slot = row_number() OVER reading THEN last_value(p.val) OVER reading
+				ELSE NULL
+				END AS rend
 			, CASE
 				WHEN d.ts < start_ts
 					THEN 0
@@ -302,13 +287,14 @@ $$
 		WHERE d.data_a IS NOT NULL
 		WINDOW slot AS (PARTITION BY p.idx ORDER BY d.ts RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
 			, slice AS (PARTITION BY p.idx ORDER BY d.ts)
+			, reading AS (PARTITION BY p.idx, CASE WHEN d.ts < end_ts THEN 0 ELSE 1 END ORDER BY d.ts RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
 	)
 	-- calculate accumulating statistics
 	, da AS (
 		SELECT
 			w.idx
 			, to_char(sum(w.diff * w.portion) FILTER (WHERE w.portion > 0), 'FM999999999999999999990.999999999')::numeric AS cdiff
-			, to_char(sum(w.diff), 'FM999999999999999999990.999999999')::numeric AS rdiff
+			, to_char(sum(w.diff) FILTER (WHERE w.ts < end_ts), 'FM999999999999999999990.999999999')::numeric AS rdiff
 			, min(w.rstart) AS rstart
 			, min(w.rend) AS rend
 		FROM wa w
@@ -351,10 +337,10 @@ $$
 		sid AS stream_id
 		, start_ts AS ts_start
 		, di_ary.data_i
-		, di_ary.stat_i
 		, da_ary.data_a
-		, da_ary.read_a
 		, ds_ary.data_s
 		, dt_ary.data_t
+		, di_ary.stat_i
+		, da_ary.read_a
 	FROM di_ary, da_ary, ds_ary, dt_ary
 $$;
