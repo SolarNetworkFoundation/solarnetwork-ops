e4c55eb93e5a0ec158e8e3f740aa9c31c086b343
Matt Magoffin <matt@solarnetwork.net>
2020-11-01 09:33:45 +1300

    Migrate NET-266 DDL to solarnetwork-central repo.

--- dba/postgres/updates/timescaledb/NET-266/postgres-init-datm.sql
+++
@@ -1,7 +0,0 @@
-\i postgres-init-datm-core.sql
-\i postgres-init-datm-in.sql
-\i postgres-init-datm-migration.sql
-\i postgres-init-loc-datm-in.sql
-\i postgres-init-loc-datm-migration.sql
-\i postgres-init-datm-query.sql
-\i postgres-init-datm-query-agg.sql
