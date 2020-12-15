c554fc4422a4e8a7fd1452b1c4d8e17cb3c8751a
Matt Magoffin <matt@solarnetwork.net>
2020-12-15 17:07:56 +1300

    Update for 2-stage upgrade, so bulk of data can be converted while SN is online.

--- dba/postgres/updates/timescaledb/NET-266/timescale-migrate-datum-to-datm-by-chunk.sh
+++ dba/postgres/updates/timescaledb/NET-266/timescale-migrate-datum-to-datm-by-chunk.sh
@@ -5,14 +5,40 @@ PORT="5432"
 USER="solarnet"
 DB="solarnetwork"
 
-while getopts ":d:h:p:U:" opt; do
+STAGE2="false"
+STAGE1_DATE="2020-11-15 00:00:00+13"
+
+do_help () {
+	cat 1>&2 <<EOF
+Usage: $0 -d <db name> -h <host> -p <port> -U <user> [-E <date>] [-2]
+
+Migrate date in two stages. In the first stage, hypertables are created and all data up to a cutoff
+date is migrated. In the second stage (by passing the -2 argument) all data after the cutoff date
+is migrated. This process is to allow for migrating most data while SolarNetwork is still online.
+The second stage is meant to be done when SolarNetwork is offline.
+
+Arguments:
+
+ -2                  - stage 2 mode; if not provided then stage 1 is assumed
+ -d <app name>       - the database name
+ -h <host>           - the host name
+ -p <port>           - the port
+ -U <user>           - the user
+ -E <date>           - the stage cutoff date, in YYYY-MM-dd HH:mm:ss+ZZ format.
+EOF
+}
+
+while getopts ":2d:E:h:p:U:" opt; do
 	case $opt in
 		d) solarnetwork="${OPTARG}";;
+		E) STAGE1_DATE="${OPTARG}";;
+		2) STAGE2='true';;
 		h) HOST="${OPTARG}";;
 		p) PORT="${OPTARG}";;
 		U) USER="${OPTARG}";;
 		*)
 			echo "Unknown argument: ${OPTARG}"
+			do_help
 			exit 1
 	esac
 done
@@ -58,36 +84,41 @@ migrate_loc_datum_range () {
 	done
 }
 
-echo `date` "Creating da_datm hypertable"
+if [ "$STAGE2" != 'true' ]; then
+	echo `date` "Creating da_datm hypertable"
 
-# initial hypertable chunk size 1 year; drop extra index first for faster insert
-psql -q -h $HOST -p $PORT -U $USER -d $DB -c \
-	"SELECT * FROM public.create_hypertable(
-	'solardatm.da_datm'::regclass,
-	'ts'::name,
-	chunk_time_interval => interval '360 days',
-	create_default_indexes => FALSE)"
+	# initial hypertable chunk size 1 year; drop extra index first for faster insert
+	psql -q -h $HOST -p $PORT -U $USER -d $DB -c \
+		"SELECT * FROM public.create_hypertable(
+		'solardatm.da_datm'::regclass,
+		'ts'::name,
+		chunk_time_interval => interval '360 days',
+		create_default_indexes => FALSE)"
 
-echo `date` "Starting datum migration"
+	echo `date` "Starting Stage 1 datum migration to ${STAGE1_DATE}..."
 
-# load oldest data into 1-year chunks
-migrate_datum_range '2009-12-01 13:00:00+13' '2015-11-01 13:00:00+13'
+	# load oldest data into 1-year chunks
+	migrate_datum_range '2009-12-01 13:00:00+13' '2015-11-01 13:00:00+13'
 
-# now bump down chunk interval to 60 days as data volume increased
-psql -q -h $HOST -p $PORT -U $USER -d $DB -c \
-	"SELECT public.set_chunk_time_interval('solardatm.da_datm', INTERVAL '60 days')"
+	# now bump down chunk interval to 60 days as data volume increased
+	psql -q -h $HOST -p $PORT -U $USER -d $DB -c \
+		"SELECT public.set_chunk_time_interval('solardatm.da_datm', INTERVAL '60 days')"
 
-# load remaining data into 60-day chunks
-migrate_datum_range '2015-11-01 13:00:00+13' '2222-01-01 00:00:00+13'
+	# load remaining data into 60-day chunks
+	migrate_datum_range '2015-11-01 13:00:00+13' "$STAGE1_DATE"
 
-# migrate location datum
-migrate_loc_datum_range '2008-01-02 01:00:00+13' '2022-01-01 00:00:00+13'
+	# migrate location datum
+	migrate_loc_datum_range '2008-01-02 01:00:00+13' "$STAGE1_DATE"
 
-# recreate dropped index
-echo `date` "Recreating da_datm_unq_reverse index"
-time psql -q -h $HOST -p $PORT -U $USER -d $DB \
-	-c \
-	"CREATE UNIQUE INDEX IF NOT EXISTS da_datm_unq_reverse
-	ON solardatm.da_datm (stream_id, ts DESC) TABLESPACE solarindex"
+	echo `date` "Finished Stage 1 datum migration to ${STAGE1_DATE}."
+else
+	echo `date` "Starting Stage 2 datum migration from ${STAGE1_DATE}..."
 
-echo `date` Finished datum migration
+	# load remaining datum
+	migrate_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13'
+
+	# migrate location datum
+	migrate_loc_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13'
+
+	echo `date` "Finished Stage 2 datum migration from ${STAGE1_DATE}."
+fi
