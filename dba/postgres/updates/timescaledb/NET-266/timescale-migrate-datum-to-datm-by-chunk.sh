15bc673cc15ec1179ca4a5d52cfe81f3cfd6da0a
Matt Magoffin <matt@solarnetwork.net>
2021-01-21 18:07:45 +1300

    Make 3 stages, so can process as much data as possible while site still online.

--- dba/postgres/updates/timescaledb/NET-266/timescale-migrate-datum-to-datm-by-chunk.sh
+++ dba/postgres/updates/timescaledb/NET-266/timescale-migrate-datum-to-datm-by-chunk.sh
@@ -5,8 +5,9 @@ PORT="5432"
 USER="solarnet"
 DB="solarnetwork"
 
-STAGE2="false"
+STAGE="1"
 STAGE1_DATE="2020-11-15 00:00:00+13"
+STAGE2_DATE="2021-01-15 00:00:00+13"
 
 do_help () {
 	cat 1>&2 <<EOF
@@ -19,21 +20,26 @@ The second stage is meant to be done when SolarNetwork is offline.
 
 Arguments:
 
- -2                  - stage 2 mode; if not provided then stage 1 is assumed
+ -2                  - stage 2 mode; if not provided, nor -3, then stage 1 is assumed
+ -3                  - stage 3 mode; if not provided, nor -2, then stage 1 is assumed
  -d <db name>        - the database name; defaults to 'solarnetwork'
  -h <host>           - the host name; defaults to 'tsdb'
  -p <port>           - the port; defaults to '5432'
  -U <user>           - the user; defaults to 'solarnet'
- -E <date>           - the stage cutoff date, in YYYY-MM-dd HH:mm:ss+ZZ format;
+ -E <date>           - the stage 1 cutoff date, in YYYY-MM-dd HH:mm:ss+ZZ format;
                        defaults to '2020-11-15 00:00:00+13'
+ -F <date>           - the stage 2 cutoff date, in YYYY-MM-dd HH:mm:ss+ZZ format;
+                       defaults to '2021-01-15 00:00:00+13'
 EOF
 }
 
-while getopts ":2d:E:h:p:U:" opt; do
+while getopts ":23d:E:F:h:p:U:" opt; do
 	case $opt in
 		d) solarnetwork="${OPTARG}";;
 		E) STAGE1_DATE="${OPTARG}";;
-		2) STAGE2='true';;
+		F) STAGE2_DATE="${OPTARG}";;
+		2) STAGE='2';;
+		3) STAGE='3';;
 		h) HOST="${OPTARG}";;
 		p) PORT="${OPTARG}";;
 		U) USER="${OPTARG}";;
@@ -48,6 +54,8 @@ shift $(($OPTIND - 1))
 migrate_datum_range () {
 	local start_date="$1"
 	local end_date="$2"
+	local min_date="${3:-min(d.ts)}"
+	local max_date="${4:-max(d.ts)}"
 	for chunk in $(psql -Aqt -h $HOST -p $PORT -U $USER -d $DB -c \
 			"SELECT chunk_table FROM chunk_relation_size_pretty('solardatum.da_datum')
 			WHERE lower(ranges[1]::tstzrange) >= '$start_date'::timestamptz
@@ -57,7 +65,7 @@ migrate_datum_range () {
 		psql -Aqt -h $HOST -p $PORT -U $USER -d $DB -F $'\t' \
 			-c '\pset pager off' \
 			-c \
-			"SELECT d.node_id, d.source_id, min(d.ts) AS ts_min, max(d.ts) AS ts_max
+			"SELECT d.node_id, d.source_id, $min_date AS ts_min, $max_date AS ts_max
 			FROM $chunk d
 			GROUP BY d.node_id, d.source_id"  2>&1 \
 			| while IFS=$'\t' read n s min max; do
@@ -74,6 +82,8 @@ migrate_datum_range () {
 migrate_loc_datum_range () {
 	local start_date="$1"
 	local end_date="$2"
+	local min_date="${3:-min(d.ts)}"
+	local max_date="${4:-max(d.ts)}"
 	for chunk in $(psql -Aqt -h $HOST -p $PORT -U $USER -d $DB -c \
 			"SELECT chunk_table FROM chunk_relation_size_pretty('solardatum.da_loc_datum')
 			WHERE lower(ranges[1]::tstzrange) >= '$start_date'::timestamptz
@@ -83,7 +93,7 @@ migrate_loc_datum_range () {
 		psql -Aqt -h $HOST -p $PORT -U $USER -d $DB -F $'\t' \
 			-c '\pset pager off' \
 			-c \
-			"SELECT d.loc_id, d.source_id, min(d.ts) AS ts_min, max(d.ts) AS ts_max
+			"SELECT d.loc_id, d.source_id, $min_date AS ts_min, $max_date AS ts_max
 			FROM $chunk d
 			GROUP BY d.loc_id, d.source_id"  2>&1 \
 			| while IFS=$'\t' read n s min max; do
@@ -97,7 +107,7 @@ migrate_loc_datum_range () {
 	done
 }
 
-if [ "$STAGE2" != 'true' ]; then
+if [ "$STAGE" = '1' ]; then
 	echo `date` "Creating da_datm hypertable"
 
 	# initial hypertable chunk size 1 year; drop extra index first for faster insert
@@ -119,20 +129,36 @@ if [ "$STAGE2" != 'true' ]; then
 #
 #	# load remaining data into 60-day chunks
 #	migrate_datum_range '2015-11-01 13:00:00+13' "$STAGE1_DATE"
+#
+#   TODO: remove following and uncomment previous
 	migrate_datum_range '2016-04-29 12:00:00+12' "$STAGE1_DATE"
 
 	# migrate location datum
 	migrate_loc_datum_range '2008-01-02 01:00:00+13' "$STAGE1_DATE"
 
 	echo `date` "Finished Stage 1 datum migration to ${STAGE1_DATE}."
-else
-	echo `date` "Starting Stage 2 datum migration from ${STAGE1_DATE}..."
+elif [ "$STAGE" = '2' ]; then
+	echo `date` "Starting Stage 2 datum migration from ${STAGE1_DATE} to ${STAGE2_DATE}..."
 
 	# load remaining datum
-	migrate_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13'
+	migrate_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13' \
+		"'${STAGE1_DATE}'::timestamptz" "'${STAGE2_DATE}'::timestamptz"
 
 	# migrate location datum
-	migrate_loc_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13'
+	migrate_loc_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13' \
+		"'${STAGE1_DATE}'::timestamptz" "'${STAGE2_DATE}'::timestamptz"
 
-	echo `date` "Finished Stage 2 datum migration from ${STAGE1_DATE}."
+	echo `date` "Finished Stage 2 datum migration from ${STAGE1_DATE} to ${STAGE2_DATE}."
+else
+	echo `date` "Starting Stage 3 datum migration from ${STAGE2_DATE}..."
+
+	# load remaining datum
+	migrate_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13' \
+		"'${STAGE2_DATE}'::timestamptz" "'2222-01-01 00:00:00+13'::timestamptz"
+
+	# migrate location datum
+	migrate_loc_datum_range "$STAGE1_DATE" '2222-01-01 00:00:00+13' \
+		"'${STAGE2_DATE}'::timestamptz" "'2222-01-01 00:00:00+13'::timestamptz"
+
+	echo `date` "Finished Stage 3 datum migration from ${STAGE2_DATE}."
 fi
