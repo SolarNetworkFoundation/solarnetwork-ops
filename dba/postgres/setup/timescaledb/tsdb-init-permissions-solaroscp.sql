dd92f491ed73e50e2b4f90b63e96f1ebdf3a292e
Matt Magoffin <matt@solarnetwork.net>
2022-08-29 15:47:17 +1200

    NET-313: update DDL.

--- dba/postgres/setup/timescaledb/tsdb-init-permissions-solaroscp.sql
+++ dba/postgres/setup/timescaledb/tsdb-init-permissions-solaroscp.sql
@@ -3,9 +3,41 @@ GRANT USAGE ON SCHEMA solaruser TO solaroscp;
 
 GRANT SELECT(user_id, node_id, archived) ON solaruser.user_node TO solaroscp;
 
+REVOKE ALL ON TABLE solaroscp.oscp_fp_token FROM solaroscp;
 REVOKE ALL ON TABLE solaroscp.oscp_fp_token FROM solaruser;
-REVOKE ALL ON TABLE solaroscp.oscp_cp_token FROM solaruser;
-REVOKE ALL ON TABLE solaroscp.oscp_co_token FROM solaruser;
 
-ALTER FUNCTION solaroscp.create_fp_token(text) SECURITY DEFINER;
+REVOKE ALL ON TABLE solaroscp.oscp_cp_token FROM solaroscp;
+REVOKE ALL ON TABLE solaroscp.oscp_cp_token FROM solaruser;
+GRANT SELECT ON TABLE solaroscp.oscp_cp_token TO solaroscp;
+
+REVOKE ALL ON TABLE solaroscp.oscp_co_token FROM solaroscp;
+REVOKE ALL ON TABLE solaroscp.oscp_co_token FROM solaruser;
+GRANT SELECT ON TABLE solaroscp.oscp_co_token TO solaroscp;
+
+REVOKE ALL ON FUNCTION solaroscp.create_fp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.create_fp_token FROM solaruser;
+ALTER FUNCTION solaroscp.create_fp_token(BIGINT, BIGINT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.update_fp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.update_fp_token FROM solaruser;
+ALTER FUNCTION solaroscp.update_fp_token(BIGINT, BIGINT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.fp_id_for_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.fp_id_for_token FROM solaruser;
 ALTER FUNCTION solaroscp.fp_id_for_token(TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.save_cp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.save_cp_token FROM solaruser;
+ALTER FUNCTION solaroscp.save_co_token(BIGINT, BIGINT, TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.save_co_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.save_co_token FROM solaruser;
+ALTER FUNCTION solaroscp.save_cp_token(BIGINT, BIGINT, TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.get_cp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.get_cp_token FROM solaruser;
+ALTER FUNCTION solaroscp.get_co_token(BIGINT, BIGINT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.get_co_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.get_co_token FROM solaruser;
+ALTER FUNCTION solaroscp.get_cp_token(BIGINT, BIGINT) SECURITY DEFINER;
--- dba/postgres/updates/timescaledb/NET-313-production.sql
+++ dba/postgres/updates/timescaledb/NET-313-production.sql
@@ -32,7 +32,30 @@ REVOKE ALL ON TABLE solaroscp.oscp_co_token FROM solaroscp;
 REVOKE ALL ON TABLE solaroscp.oscp_co_token FROM solaruser;
 GRANT SELECT ON TABLE solaroscp.oscp_co_token TO solaroscp;
 
+REVOKE ALL ON FUNCTION solaroscp.create_fp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.create_fp_token FROM solaruser;
 ALTER FUNCTION solaroscp.create_fp_token(BIGINT, BIGINT) SECURITY DEFINER;
-ALTER FUNCTION solaroscp.create_cp_token(BIGINT, BIGINT) SECURITY DEFINER;
-ALTER FUNCTION solaroscp.create_co_token(BIGINT, BIGINT) SECURITY DEFINER;
-ALTER FUNCTION solaroscp.fp_id_for_token(TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.update_fp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.update_fp_token FROM solaruser;
+ALTER FUNCTION solaroscp.update_fp_token(BIGINT, BIGINT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.fp_id_for_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.fp_id_for_token FROM solaruser;
+ALTER FUNCTION solaroscp.fp_id_for_token(TEXT, BOOLEAN) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.save_cp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.save_cp_token FROM solaruser;
+ALTER FUNCTION solaroscp.save_co_token(BIGINT, BIGINT, TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.save_co_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.save_co_token FROM solaruser;
+ALTER FUNCTION solaroscp.save_cp_token(BIGINT, BIGINT, TEXT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.get_cp_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.get_cp_token FROM solaruser;
+ALTER FUNCTION solaroscp.get_co_token(BIGINT, BIGINT) SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION solaroscp.get_co_token FROM PUBLIC;
+REVOKE ALL ON FUNCTION solaroscp.get_co_token FROM solaruser;
+ALTER FUNCTION solaroscp.get_cp_token(BIGINT, BIGINT) SECURITY DEFINER;
