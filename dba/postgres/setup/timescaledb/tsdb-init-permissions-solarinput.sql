b1c39490aed9d6d2028ab1c6ca2543fef99cd4af
Matt Magoffin <matt@solarnetwork.net>
2022-11-21 09:30:01 +1300

    Add missing permissions for solarinput.

--- dba/postgres/setup/timescaledb/tsdb-init-permissions-solarinput.sql
+++ dba/postgres/setup/timescaledb/tsdb-init-permissions-solarinput.sql
@@ -4,6 +4,8 @@ GRANT SELECT ON TABLE solaruser.user_user TO solarinput;
 
 GRANT ALL ON SEQUENCE solarev.ocpp_charge_tx_seq TO solarinput;
 GRANT SELECT, UPDATE, REFERENCES, TRIGGER ON TABLE solarev.ocpp_charge_point TO solarinput;
+GRANT ALL ON TABLE solarev.ocpp_charge_point_action_status TO solarinput;
 GRANT ALL ON TABLE solarev.ocpp_charge_point_conn TO solarinput;
+GRANT ALL ON TABLE solarev.ocpp_charge_point_status TO solarinput;
 GRANT ALL ON TABLE solarev.ocpp_charge_sess TO solarinput;
 GRANT ALL ON TABLE solarev.ocpp_charge_sess_reading TO solarinput;
