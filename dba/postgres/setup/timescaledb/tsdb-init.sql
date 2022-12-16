a518f719c3d7f133f62b9202b287e107f5dcc120
Matt Magoffin <matt@solarnetwork.net>
2022-12-16 22:38:56 +1300

    NET-303: reorder for intra-deps.

--- dba/postgres/setup/timescaledb/tsdb-init.sql
+++ dba/postgres/setup/timescaledb/tsdb-init.sql
@@ -17,6 +17,8 @@
 \i init/postgres-init-datm-schema.sql
 \i tsdb-init-datm-schema.sql
 \i init/postgres-init-datm-core.sql
+\i init/postgres-init-datm-util.sql
+\i init/postgres-init-datm-query.sql
 \i init/postgres-init-datm-agg-query.sql
 \i init/postgres-init-datm-agg.sql
 \i init/postgres-init-datm-agg-util.sql
@@ -25,8 +27,6 @@
 \i init/postgres-init-datm-delete.sql
 \i init/postgres-init-datm-in.sql
 \i init/postgres-init-datm-in-loc.sql
-\i init/postgres-init-datm-util.sql
-\i init/postgres-init-datm-query.sql
 \i init/postgres-init-datm-query-agg.sql
 \i init/postgres-init-datm-query-diff.sql
 \i init/postgres-init-datum-export.sql
