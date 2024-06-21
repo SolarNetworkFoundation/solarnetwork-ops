1b26d129c8a0381aa4734f977ea9fe447d8d6b53
Matt Magoffin <matt@solarnetwork.net>
2024-06-21 14:24:40 +1200

    NET-382: add table ACL.

--- dba/postgres/updates/timescaledb/NET-382-http-sessions-production.sql
+++ dba/postgres/updates/timescaledb/NET-382-http-sessions-production.sql
@@ -5,3 +5,9 @@ ALTER INDEX solaruser.http_session_session_unq 		SET TABLESPACE solarindex;
 ALTER INDEX solaruser.http_session_exp_idx 			SET TABLESPACE solarindex;
 ALTER INDEX solaruser.http_session_principal_idx 	SET TABLESPACE solarindex;
 ALTER INDEX solaruser.http_session_attributes_pk 	SET TABLESPACE solarindex;
+
+REVOKE ALL ON TABLE solaruser.http_session FROM solar;
+GRANT ALL ON TABLE solaruser.http_session TO solaruser;
+
+REVOKE ALL ON TABLE solaruser.http_session_attributes FROM solar;
+GRANT ALL ON TABLE solaruser.http_session_attributes TO solaruser;
