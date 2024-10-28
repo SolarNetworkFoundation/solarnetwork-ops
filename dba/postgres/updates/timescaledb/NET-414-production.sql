08eba5dbaebc4c11759eb78eba0fffd60415772c
Matt Magoffin <matt@solarnetwork.net>
2024-10-29 12:05:16 +1300

    NET-414: production DDL.

---
+++ dba/postgres/updates/timescaledb/NET-414-production.sql
@@ -0,0 +1,4 @@
+\i init/updates/NET-414-cloud-integrations-flux.sql
+
+ALTER INDEX solardin.cin_user_settings_pk 					SET TABLESPACE solarindex;
+ALTER INDEX solardin.cin_datum_stream_settings_pk 			SET TABLESPACE solarindex;
