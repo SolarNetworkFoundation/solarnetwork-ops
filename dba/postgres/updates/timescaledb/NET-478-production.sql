8b47b01dd74889b2429f87208ab53ddf4f2f4343
Matt Magoffin <matt@solarnetwork.net>
2025-09-25 08:41:16 +1200

    NET-478: add production DDL.

---
+++ dba/postgres/updates/timescaledb/NET-478-production.sql
@@ -0,0 +1,4 @@
+\i init/updates/NET-478-cloud-datum-stream-rake.sql
+
+ALTER INDEX solardin.cin_datum_stream_rake_task_pk SET TABLESPACE solarindex;
+ALTER INDEX solardin.cin_datum_stream_rake_task_exec_idx SET TABLESPACE solarindex;
